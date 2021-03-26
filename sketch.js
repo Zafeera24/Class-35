8019b5f2c31538fb5df43c292aa6b7ab308bad81
Zafeera24 <72991213+Zafeera24@users.noreply.github.com>
2021-03-26 18:08:50 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var ball, store;
+var Position, database;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database = firebase.database();
+
+    var ballposition = database.ref("ball/position");
+    ballposition.on("value" ,read)
+}
+
+function draw(){
+    background("aqua");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+
+    database.ref("ball/position").set({
+        x: ball.x,
+        y: ball.y 
+    })
+}
+
+function read(data){
+    store = data.val();
+    console.log(store);
+    ball.x = store.x;
+    ball.y = store.y;
+}
+
+
